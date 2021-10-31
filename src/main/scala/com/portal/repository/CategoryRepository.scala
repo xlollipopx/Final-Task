8e24fdaa11b42e10bbb241208aa6a0924f49f184
xlollipopx <anton.stelmax@bk.ru>
2021-10-31 23:05:07 +0300

    specific products added

--- src/main/scala/com/portal/repository/CategoryRepository.scala
+++ src/main/scala/com/portal/repository/CategoryRepository.scala
@@ -1,3 +1,17 @@
-package com.portal.repository object CategoryRepository {
+package com.portal.repository
 
+import cats.effect.Sync
+import com.portal.domain.category.Category
+import com.portal.dto.product.CategoryDto
+import com.portal.repository.impl.doobie.DoobieCategoryRepository
+import doobie.Transactor
+
+trait CategoryRepository[F[_]] {
+  def all(): F[List[Category]]
+  def create(category: Category): F[Int]
+  def update(category: Category): F[Int]
+}
+
+object CategoryRepository {
+  def of[F[_]: Sync](tx: Transactor[F]): DoobieCategoryRepository[F] = new DoobieCategoryRepository[F](tx)
 }
