ff498e79935698b617c84419f893c35d362e7fac
xlollipopx <anton.stelmax@bk.ru>
2021-10-31 23:05:30 +0300

    specific products added

--- src/main/scala/com/portal/service/CategoryService.scala
+++ src/main/scala/com/portal/service/CategoryService.scala
@@ -1,3 +1,22 @@
-package com.portal.service object CategoryService {
+package com.portal.service
 
+import cats.effect.Sync
+import com.portal.domain.category.Category
+import com.portal.dto.product.{CategoryDto, CategoryDtoModify}
+import com.portal.repository.CategoryRepository
+import com.portal.service.impl.CategoryServiceImpl
+
+import java.util.UUID
+
+trait CategoryService[F[_]] {
+  def all(): F[List[CategoryDto]]
+  def create(category: CategoryDtoModify): F[CategoryDto]
+  def update(id:       UUID, category: CategoryDtoModify): F[CategoryDto]
+}
+
+object CategoryService {
+  def of[F[_]: Sync](
+    categoryRepository: CategoryRepository[F]
+  ): CategoryServiceImpl[F] =
+    new CategoryServiceImpl[F](categoryRepository)
 }
