a558007d7e49891e31ea1c266c4fcded038ac4c5
xlollipopx <anton.stelmax@bk.ru>
2021-10-31 23:05:19 +0300

    specific products added

--- src/main/scala/com/portal/http/routes/CategoryRoutes.scala
+++ src/main/scala/com/portal/http/routes/CategoryRoutes.scala
@@ -1,3 +1,27 @@
-package com.portal.http.routes class CategoryRoutes {
+package com.portal.http.routes
 
+import com.portal.service.CategoryService
+import cats.Monad
+import cats.effect.Sync
+import org.http4s.HttpRoutes
+import org.http4s.dsl.Http4sDsl
+import cats.implicits._
+import io.circe.generic.auto._
+import org.http4s.circe.CirceEntityCodec.{circeEntityDecoder, circeEntityEncoder}
+
+import org.http4s.server.Router
+
+final case class CategoryRoutes[F[_]: Monad: Sync](categoryService: CategoryService[F]) extends Http4sDsl[F] {
+  private val prefixPath = "/products"
+
+  private val httpRoutes: HttpRoutes[F] = HttpRoutes.of[F] { case GET -> Root / "categories" / "all" =>
+    for {
+      list <- categoryService.all()
+      res  <- Ok(list)
+    } yield res
+  }
+
+  val routes: HttpRoutes[F] = Router(
+    prefixPath -> httpRoutes
+  )
 }
