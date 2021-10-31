9118dbdc052d2f2ce34c27d2a0d32a1f232aba33
xlollipopx <anton.stelmax@bk.ru>
2021-10-31 23:05:51 +0300

    specific products added

--- src/main/scala/com/portal/validation/CategoryValidator.scala
+++ src/main/scala/com/portal/validation/CategoryValidator.scala
@@ -1,3 +1,16 @@
-package com.portal.validation object CategoryValidator {
+package com.portal.validation
+
+import com.portal.dto.product.CategoryDto
+
+sealed trait CategoryValidatorError
+object CategoryValidatorError {
+
+  final case object InvalidId extends CategoryValidatorError {
+    override def toString: String = "Wrong id!"
+  }
+
+  final case object EmptyName extends CategoryValidatorError {
+    override def toString: String = "Name must be nonempty!"
+  }
 
 }
